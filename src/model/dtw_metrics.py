036dd878e81fb2be5ca66a3d1516dd2e06800137
Mukund Shankar <smukund23@gmail.com>
2024-07-19 10:51:39 -0400

    fixed metrics, still need to check up on m2t

--- src/model/dtw_metrics.py
+++ src/model/dtw_metrics.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+# Get all metrics - t2m, m2t for a given similarity matrix and protocol (determined when called)
 def all_contrastive_metrics(
     sims, keyids, ref_df, dtw_scores, emb=None, threshold=None, rounding=2, return_cols=False
 ):
+    # emb decides if it's protocol (b) or not (use of sentence embeddings)
     text_selfsim = None
     if emb is not None:
         text_selfsim = emb @ emb.T
@@ -13,9 +15,6 @@ def all_contrastive_metrics(
     m2t_m, m2t_cols, top_m2t_retrievals = contrastive_metrics(
         sims.T, keyids, ref_df, dtw_scores, text_selfsim=text_selfsim, threshold=threshold, return_cols=True, rounding=rounding
     )
-
-    # keyid_metrics = {"t2m": t2m_keyid_metrics, "m2t": m2t_keyid_metrics}
-    # retrieved = {"t2m_retrievals": top_t2m_retrievals, "m2t_retrievals": top_m2t_retrievals}
 
     all_m = {}
     for key in t2m_m:
@@ -28,7 +27,6 @@ def all_contrastive_metrics(
         return all_m, t2m_cols, m2t_cols
     return all_m, top_t2m_retrievals, top_m2t_retrievals
 
-
 def contrastive_metrics(
     sims,
     keyids,
@@ -40,6 +38,7 @@ def contrastive_metrics(
     rounding=2,
     break_ties="averaging",
 ):
+    # Convert keyids to a np array for easier querying
     keyids = np.array(keyids)
     n, m = sims.shape
     assert n == m
@@ -52,31 +51,37 @@ def contrastive_metrics(
 
     top_retrieval_indices = np.argsort(dists, axis=1)[:, :10]
 
+    # Go over what this does exactly
     if text_selfsim is not None and threshold is not None:
+        # Get threshold to be between -1 and 1
         real_threshold = 2 * threshold - 1
+        # Take where the text sim matrix is above this threshold
         idx = np.argwhere(text_selfsim > real_threshold)
+        
+        # From GPT:
+        # The code finds unique row indices from idx and their first occurrences.
+        # It then computes the minimum distances in dists for these unique rows using a partitioned reduction operation.
+        # Finally, it reshapes the resulting array of minimum distances into a column vector.
         partition = np.unique(idx[:, 0], return_index=True)[1]
         # take as GT the minimum score of similar values
         gt_dists = np.minimum.reduceat(dists[tuple(idx.T)], partition)
         gt_dists = gt_dists[:, None]
 
+    # Check up what rows and cols are
     rows, cols = np.where((sorted_dists - gt_dists) == 0)  # find column position of GT
-
+    
+    # Write a test case for this
     for i, row in enumerate(top_retrieval_indices):
-        # breakpoint()
         row_keyids = keyids[row]
         mask = ref_df['keyids'].isin(row_keyids)
         matching_indices = ref_df.index[mask].tolist()
         row_dtw_scores = np.array(dtw_scores[i][matching_indices])
         try:
-            best_row = np.min(np.where(row_dtw_scores < 150))
-            if best_row < cols[i]:
-                cols[i] = best_row
+            best_col = np.min(np.where(row_dtw_scores < 200)[1])
+            if best_col < cols[i]:
+                cols[i] = best_col
         except:
             pass
-        # breakpoint()
-
-    # breakpoint()
 
     # if there are ties
     if rows.size > num_queries:
@@ -96,7 +101,6 @@ def contrastive_metrics(
         return metrics, cols, top_retrieval_indices
     return metrics
 
-
 def break_ties_average(sorted_dists, gt_dists):
     # fast implementation, based on this code:
     # https://stackoverflow.com/a/49239335
@@ -113,43 +117,11 @@ def break_ties_average(sorted_dists, gt_dists):
     avg_cols = summed_cols / counts
     return avg_cols
 
-
 def break_ties_optimistically(sorted_dists, gt_dists):
     rows, cols = np.where((sorted_dists - gt_dists) == 0)
     _, idx = np.unique(rows, return_index=True)
     cols = cols[idx]
     return cols
-
-
-# def cols2metrics(cols, num_queries, keyids, rounding=2):
-#     metrics = {}
-#     vals = [str(x).zfill(2) for x in [1, 2, 3, 5, 10]]
-#     for val in vals:
-#         metrics[f"R{val}"] = 100 * float(np.sum(cols < int(val))) / num_queries
-
-#     metrics["MedR"] = float(np.median(cols) + 1)
-
-#     all_keyid_metrics = {}
-#     for idx, keyid in enumerate(keyids):
-
-#         keyid_metrics = {"R01": False, "R02": False, "R03": False, "R05": False, "R10": False}
-#         keyid_val = cols[idx]
-#         if keyid_val < 1:
-#             keyid_metrics["R01"] = True
-#         elif keyid_val < 2:
-#             keyid_metrics["R02"] = True
-#         elif keyid_val < 3:
-#             keyid_metrics["R03"] = True
-#         elif keyid_val < 5:
-#             keyid_metrics["R05"] = True
-#         elif keyid_val < 10:
-#             keyid_metrics["R10"] = True
-#         all_keyid_metrics[keyid] = keyid_metrics
-
-#     if rounding is not None:
-#         for key in metrics:
-#             metrics[key] = round(metrics[key], rounding)
-#     return metrics, all_keyid_metrics
 
 def cols2metrics(cols, num_queries, keyids, rounding=2):
     metrics = {}
